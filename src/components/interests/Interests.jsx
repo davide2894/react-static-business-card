6220afb81d0464a84e576beb4c3c1c3e782f8d21
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-05-26 20:39:01 +0200

    refactor: improve style code

--- src/components/interests/Interests.jsx
+++ src/components/interests/Interests.jsx
@@ -3,8 +3,8 @@ import './Interests.scss';
 export default function Interests() {
     return (
         <div className="interests businessCard__child businessCard__margins">
-            <h3>Interests</h3>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <h3 className='businessCard__h3'>Interests</h3>
+            <p className='businessCard__p'>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
     )
 }
